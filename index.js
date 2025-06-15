53981da814ea7e8fd951db04c38f0535db4c77a6
Srikanthbolle <bollesrikanth186@gmail.com>
2025-06-15 09:37:00 +0530

    created mongodb

---
+++ index.js
@@ -0,0 +1,17 @@
+// index.js
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/mongodb.js";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("API running...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
