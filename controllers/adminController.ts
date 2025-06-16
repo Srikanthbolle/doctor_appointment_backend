91300919ec1dcad42193e1453a74de89175e9a42
Srikanthbolle <bollesrikanth186@gmail.com>
2025-06-16 07:49:22 +0530

    created a model

---
+++ controllers/adminController.ts
@@ -0,0 +1,12 @@
+
+
+
+const addDoctor = async(req,res)=>{
+ try {
+    const {name,email,password,speciality,degree,expreince,about,fees,address} =req.body
+ } catch (error) {
+    
+ }
+}
+
+export {addDoctor}
